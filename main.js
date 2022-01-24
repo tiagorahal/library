bd03393c42c55eb187ce36c813b6debc5319fd7e
“tiagorahal” <rahal.aires@gmail.com>
2022-01-24 18:21:32 -0300

    Add new Book instance

--- main.js
+++ main.js
@@ -52,3 +52,19 @@ class Book extends Media {
   }
 
 }
+
+class Movie extends Media {
+  constructor(director, title, runTime) {
+    super(title)
+    this._director = director;
+    this._runTime = runTime
+  }
+
+  get director() {
+    return this._director;
+  }
+
+  get runTime() {
+    return this._runTime;
+  }
+}
